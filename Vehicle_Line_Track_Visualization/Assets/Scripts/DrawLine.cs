486276e0ea01699ecff0667a36081903fca3a994
Tristana_Dragon_Trainer <48664307+DragonTrainerTristana@users.noreply.github.com>
2022-12-20 18:27:59 +0900

    Trajectory
    제곧내

--- Vehicle_Line_Track_Visualization/Assets/Scripts/DrawLine.cs
+++ Vehicle_Line_Track_Visualization/Assets/Scripts/DrawLine.cs
@@ -4,20 +4,45 @@ using UnityEngine;
 
 public class DrawLine : MonoBehaviour 
 {
+
+    LineRenderer lineRenderer;
+
     public GameObject[] checkPoint;
+    private int count;
+    private int lengthOfLineRenderer = 10000;
+
+
+    private float OriginalTime;
+    private float criteriaTime;
 
     void Start()
     {
-        
+        count = 0;
+        OriginalTime = 0.0f;
+
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startWidth = .05f;
+        lineRenderer.endWidth = .05f;
+        lineRenderer.SetVertexCount(lengthOfLineRenderer);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        OriginalTime += Time.deltaTime;
         checkPoint = GameObject.FindGameObjectsWithTag("CheckPoint");
-        if (checkPoint.Length > 1) {
+        if (checkPoint.Length > 1 && OriginalTime >= criteriaTime) {
+            OriginalTime = 0;
             for (int i = 0; i < checkPoint.Length - 1; i++) {
-                Debug.DrawLine(checkPoint[i].transform.position, checkPoint[i + 1].transform.position, Color.green);
+
+
+
+                lineRenderer.SetPosition(count, checkPoint[i].transform.position);
+                lineRenderer.SetPosition(count + 1, checkPoint[i + 1].transform.position);
+                count++;
+
+                //Debug.Log("그림그리기");
+                //Debug.DrawLine(checkPoint[i].transform.position, checkPoint[i + 1].transform.position, Color.green);
             }
         }
         
--- Vehicle_Line_Track_Visualization/Assets/Scripts/MakeLine.cs
+++ Vehicle_Line_Track_Visualization/Assets/Scripts/MakeLine.cs
@@ -8,16 +8,28 @@ public class MakeLine : MonoBehaviour
     GameObject car;
 
 
+    // Time Component
+    private float OriginalTime;
+    private float criteriaTime;
+
     void Start()
     {
+        OriginalTime = 0.0f;
+        criteriaTime = 1.0f;
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        car = new GameObject("CheckPoint");
-        car.tag = "CheckPoint";
-        car.transform.position = this.transform.position;
+        OriginalTime += Time.deltaTime;
+
+        if (OriginalTime >= criteriaTime) {
+            OriginalTime = 0;
+            car = new GameObject("CheckPoint");
+            car.tag = "CheckPoint";
+            car.transform.position = this.transform.position;
+        }
+       
     }
 }
